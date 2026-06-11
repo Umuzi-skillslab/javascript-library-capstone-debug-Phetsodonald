474661add78b65b7e9760c92a141aa4fb0c7a528
Phetso <phetso.nkosi@umuzi.org>
2026-06-11 20:17:42 +0200

    added super to inherit book properties

--- library.js
+++ library.js
@@ -54,7 +54,9 @@ class Book {
 // Digital book class with inheritance problems
 class DigitalBook extends Book {
     constructor(isbn, title, author, year, fileSize, format) {
-        // Missing: super() call with correct parameters
+        
+        super(isbn, title, author, year);
+
         this.fileSize = fileSize;
         this.format = format;
         this.downloads = 0;
